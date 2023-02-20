9a1f7922f09cec3d619747128492d1dcf993dfe0
Rafael Nunes <rafael.nunes@softdesign.com.br>
2023-02-20 15:43:56 -0300

    change atlas mongo url to docker local url

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
 import { userController } from "../controllers/user.controller";
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-
 export async function conn() {
   try {
     mongoose.set("strictQuery", false);
 
-    await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.lzpxpaf.mongodb.net/?retryWrites=true&w=majority`
-    );
+    await mongoose.connect(process.env.DB_URL!);
     console.log("Mongo connected");
 
     await userController.createAdminUser();
